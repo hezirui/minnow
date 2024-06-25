208833ffa9387b2a10c3b0c816d8341edbd26076
hzr <1033387948@qq.com>
2024-06-25 08:29:44 +0000

    check0完成 在构造函数中，使用列表初始化变量，必须按照变量在类中声明的顺序来初始化

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,17 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket;
+  Address addr(host,"http");
+  socket.connect(addr);
+  string req = "GET " + path + " HTTP/1.1" + "\r\n" + "Host: " + host + "\r\n" + "Connection: close" + "\r\n\r\n\r\n";
+  socket.write(req);
+  socket.shutdown(SHUT_WR);
+  string buffer;
+  while(!socket.eof()){
+    socket.read(buffer);
+    cout << buffer;
+  }
 }
 
 int main( int argc, char* argv[] )
--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,64 +2,81 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), error_(false), closed_(false), byte_stream_(), available_capacity_(capacity_), bytes_pushed_(0), bytes_buffered_(0), bytes_popped_(0){}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return closed_;;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
+  if(is_closed())
+    return;
+  if(data.length() <= available_capacity()){//正常情况
+    byte_stream_ += data;
+    available_capacity_ -= data.length();
+    bytes_pushed_ += data.length();
+    bytes_buffered_ += data.length();
+  }else{//data的长度大于 available_capacity_
+    byte_stream_ += data.substr(0,available_capacity());
+    bytes_pushed_ += available_capacity();
+    bytes_buffered_ += available_capacity();
+    available_capacity_ = 0;
+  }
   return;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return available_capacity_;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  if(byte_stream_.empty() && writer().is_closed())
+    return true;
+  return false;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  return byte_stream_;
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  if(is_finished())
+    return;
+  if( len < bytes_buffered_ ){//正常情况
+    bytes_buffered_ -= len;
+    available_capacity_ += len;
+    bytes_popped_ += len;
+    byte_stream_ = byte_stream_.substr(len);
+  }else{//len比缓冲字节大
+    available_capacity_ = capacity_;
+    bytes_popped_ += bytes_buffered_;
+    bytes_buffered_ = 0;
+    byte_stream_.clear();
+  }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return bytes_buffered_;
 }
